da6e37ae1648886af25151586c48c8fa837e0877
aprmsd <aprmsd@gmail.com>
2023-01-06 23:58:43 +0530

    basics

---
+++ Take You Forward/cpp-basics/2-data-types.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    //------------
+    // Rounding limits for each data type
+    // INT : -10^9 to 10^9
+    // LONG : -10^12 to 10^12
+    // LONG LONG : -10^18 to 10^18
+
+    //------------
+
+    // int
+    int x = 10;
+
+    // long
+    long xl = 15;
+
+    long long xll = 150000000;
+
+    // float, double
+
+    float xf = 5.6;
+    float yf = 5;
+
+    double a = 6;
+    double b = 11.25555;
+
+    // string and getline
+
+    string s1;
+    string s2;
+    // cin >> s1 >> s2;
+    // cout << s1 << " " << s2;
+
+    // get line
+
+    string str;
+    // getline(cin, str); // get one line string, if you press enter that won't be noticed
+    // cout << str;
+
+    // character
+    char ch;
+    cin >> ch;
+    cout << ch;
+    return 0;
+}
---
+++ Take You Forward/cpp-basics/3-if-else.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // print if you are adult or not
+
+    int age;
+    cin >> age;
+
+    if (age >= 18)
+    {
+        cout << "Adult";
+    }
+    else
+    {
+        cout << "Not Adult";
+    }
+
+    return 0;
+}
